6319e436693f175729b2968edaeda25190d0e096
Nicolas Del Vecchio <110258555+caapuu-nico@users.noreply.github.com>
2024-11-26 11:55:31 -0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const server = require("./server");
-const {main} = require("./db")
-const PORT = 3001;
-
-main().catch(err=>console.log(err));
-server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-})
